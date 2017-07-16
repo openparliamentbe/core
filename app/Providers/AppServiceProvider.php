d42bfae26b94e1563846a7cf44f6524c772883fa
michael <michael@estsurinter.net>
2017-07-17 00:48:12 +0200

    Configure the default string length for migrations
    By default, Laravel uses the `utf8mb4` character set, allowing to store characters from any Unicode plane (and not only characters from the Basic Multilingual Plane).
    
    However, this may create some trouble with some installations. This is a fix for those situations.
    
    See https://laravel.com/docs/5.4/migrations#indexes fro more details about the issue.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // Set the default string length for database migrations.
+        // @see https://laravel.com/docs/5.4/migrations#indexes
+        Schema::defaultStringLength(191);
     }
 
     /**
