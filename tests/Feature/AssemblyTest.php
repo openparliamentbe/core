1e461b879f56a475778116adf741b312745c1baf
michael <michael@estsurinter.net>
2017-07-21 23:33:13 +0200

    Add feature tests for getting a single assembly

--- tests/Feature/AssemblyTest.php
+++ tests/Feature/AssemblyTest.php
@@ -92,4 +92,65 @@ class AssemblyTest extends TestCase
             ->assertContentType('text/xml')
             ->assertContentEquals(__DIR__.'/fixtures/assemblies.xml');
     }
+
+    /** @test */
+    function can_get_a_single_assembly_as_json()
+    {
+        // Make the request.
+        $response = $this->get('/assemblies/k', [
+            'accept' => 'application/json',
+        ]);
+
+        // Verify the response.
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertContentType('application/json')
+            ->assertContentEquals(__DIR__.'/fixtures/single_assembly.json');
+    }
+
+    /** @test */
+    function can_get_a_single_assembly_as_csv()
+    {
+        // Make the request.
+        $response = $this->get('/assemblies/k', [
+            'accept' => 'text/csv',
+        ]);
+
+        // Verify the response.
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertContentType('text/csv')
+            ->assertContentEquals(__DIR__.'/fixtures/single_assembly.csv');
+    }
+
+    /** @test */
+    function can_get_a_single_assembly_as_html()
+    {
+        // Make the request.
+        $response = $this->get('/assemblies/k', [
+            'accept' => 'text/html',
+        ]);
+
+        // Verify the response.
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertContentType('text/html')
+            ->assertSeeText('Chambre des représentants')
+            ->assertDontSeeText('Sénat');
+    }
+
+    /** @test */
+    function can_get_a_single_assembly_as_xml()
+    {
+        // Make the request.
+        $response = $this->get('/assemblies/k', [
+            'accept' => 'text/xml',
+        ]);
+
+        // Verify the response.
+        $response
+            ->assertStatus(Response::HTTP_OK)
+            ->assertContentType('text/xml')
+            ->assertContentEquals(__DIR__.'/fixtures/single_assembly.xml');
+    }
 }
